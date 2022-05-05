8c44e33fbbc315082f169c21804f12438525a719
Michael Melanson <michael.melanson@clear.co>
2022-05-05 14:03:44 -0400

    feat: Add partial Rust/Warp implementation

---
+++ rust/src/payload.rs
@@ -0,0 +1,20 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Payload {
+    field1: String,
+    field2: String,
+    field3: String,
+    field4: String,
+    field5: String,
+    detail: Vec<PayloadDetail>
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct PayloadDetail {
+    field6: f64,
+    field7: f64,
+    field8: f64,
+    field9: f64,
+    field10: f64,
+}
